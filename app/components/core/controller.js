a23ddac3afee6be07375264c67aff3b56f4b5a6e
Gabriel Costa <gpc3@icomp.ufam.edu.br>
2017-09-17 22:55:30 -0400

    Atualizando código

--- app/components/core/controller.js
+++ app/components/core/controller.js
@@ -1,21 +1,93 @@
 let model = require('../index');
+const swipl = require('swipl');
+
+swipl.call(`consult("C:/Users/Vostro/Documents/workspace/ufam/IA/IA-GeographyBot/app/components/core/knowledge.pl")`);
+
+function process(command, value) {
+    let query = new swipl.Query(`${command}("${value}", Y)`);
+    console.log(command, value);
+    let ret = null;
+    while (ret = query.next()) {
+        if(ret != null)
+            console.log(ret);
+        query.close();
+        return ret.Y;
+    }
+
+    query.close();
+    return null;
+}
 
 module.exports = function() {
     const TelegramBot = require('node-telegram-bot-api');
     const token = '385837953:AAGoqbe3ZYqUS5ijCOr_sw5BGiM5VIAj3cA';
     const bot = new TelegramBot(token, {polling: true});
 
-    bot.onText(/\/capital (.+)/, (msg, match) => {
-        const chatId = msg.chat.id;
-        const resp = match[1]; 
-        console.log(msg, match);
+    bot.onText(/\/help/, (msg, match) => { 
+        const chatId = msg.chat.id; 
+        let resp = "Muito bom meu caro. Irei falar algumas estruturas de perguntas que entendo atualmente.";
+        bot.sendMessage(chatId, resp); 
+        resp = "Não me julgue T_T";
+        bot.sendMessage(chatId, resp);
+        resp = `Pergunte "Qual é a capital do estado X? ou então Qual é o tamanho do estado Y? ou Qual é a comida típica da região Z?`;
         bot.sendMessage(chatId, resp);
     });
 
-    bot.on('message', (msg) => {
-        const chatId = msg.chat.id;
-        
-        console.log(msg);
-        bot.sendMessage(chatId, 'Received your message');
+    bot.onText(/^(?:Q|q)ual (?:é )?a? (capital) d(?:o estado (?:(?:d(?:e|a|o)))?|(?:o|a)) ((?:[^?])+)+\??/, (msg, match) => { 
+        const chatId = msg.chat.id; 
+        let resp = process(match[1], match[2].toLowerCase());
+
+        if(resp != null) {
+            resp = 'Acredito fortemente que é ' + resp;
+        }
+        else {
+            resp = 'Entendi a pergunta, mas... Não lembro agora xD';
+        }
+        bot.sendMessage(chatId, resp); 
     });
+
+    bot.onText(/^(?:Q|q)ual (?:é )?o? (tamanho) d(?:o estado (?:(?:d(?:e|a|o) ))?|(?:o|a) )((?:[^?])+)+\??/, (msg, match) => { 
+        const chatId = msg.chat.id; 
+        let resp = process(match[1], match[2].toLowerCase());
+
+        if(resp != null) {
+            resp = 'Acredito fortemente que é ' + resp + "*1000 km²";
+        }
+        else {
+            resp = 'Entendi a pergunta, mas... Não lembro agora xD';
+        }
+        bot.sendMessage(chatId, resp); 
+    });
+
+    bot.onText(/^(?:Q|q)ua(?:l|is) (?:é |são )?(?:a?(?:s)?)? (comida) (?:típica) d(?:(?:o|a )(?:região )?)((?:[^?])+)+\??/, (msg, match) => { 
+        const chatId = msg.chat.id; 
+        let resp = process(match[1], match[2].toLowerCase());
+
+        if(resp != null) {
+            resp = 'Acredito fortemente que é ' + resp;
+        }
+        else {
+            resp = 'Entendi a pergunta, mas... Não lembro agora xD';
+        }
+        bot.sendMessage(chatId, resp); 
+    });
+
+    bot.onText(/^(?:Q|q)ual (?:é )?o? (pib) d(?:o estado (?:(?:d(?:e|a|o)))?|(?:o|a)) ((?:[^?])+)+\??/, (msg, match) => { 
+        const chatId = msg.chat.id; 
+        let resp = process(match[1], match[2].toLowerCase());
+
+        if(resp != null) {
+            resp = 'Acredito fortemente que é ' + resp + " (valor em bilhões)";
+        }
+        else {
+            resp = 'Entendi a pergunta, mas... Não lembro agora xD';
+        }
+        bot.sendMessage(chatId, resp); 
+    });
+
+    // bot.on('message', (msg) => {
+    //     const chatId = msg.chat.id;
+
+    //     bot.sendMessage(chatId, processText(msg.text.toLowerCase()));
+    // });
 }
--- app/components/core/knowledge.pl
+++ app/components/core/knowledge.pl
@@ -1,55 +1,117 @@
-estado("Acre").
-estado("Alagoas").
-estado("Amapá").
-estado("Amazonas").
-estado("Bahia").
-estado("Ceará").
-estado("Distrito Federal").
-estado("Espírito Santo").
-estado("Goiás").
-estado("Maranhão").
-estado("Mato Grosso").
-estado("Mato Grosso do Sul").
-estado("Minas Gerais").
-estado("Pará").
-estado("Paraíba").
-estado("Paraná").
-estado("Pernambuco").
-estado("Piauí").
-estado("Rio de Janeiro").
-estado("Rio Grande do Norte").
-estado("Rio Grande do Sul").
-estado("Rondônia").
-estado("Roraima").
-estado("Santa Catarina").
-estado("São Paulo").
-estado("Sergipe").
-estado("Tocantins").
+estado("acre").
+estado("alagoas").
+estado("amapá").
+estado("amazonas").
+estado("bahia").
+estado("ceará").
+estado("distrito federal").
+estado("espírito santo").
+estado("goiás").
+estado("maranhão").
+estado("mato grosso").
+estado("mato grosso do sul").
+estado("minas gerais").
+estado("pará").
+estado("paraíba").
+estado("paraná").
+estado("pernambuco").
+estado("piauí").
+estado("rio de janeiro").
+estado("rio grande do norte").
+estado("rio grande do sul").
+estado("rondônia").
+estado("roraima").
+estado("santa catarina").
+estado("são paulo").
+estado("sergipe").
+estado("tocantins").
 
-capital("Acre", "Rio Branco").
-capital("Alagoas", "Maceió").
-capital("Amapá", "Macapá").
-capital("Amazonas", "Manaus").
-capital("Bahia", "Salvador").
-capital("Ceará", "Fortaleza").
-capital("Distrito Federal", "Brasília").
-capital("Espírito Santo", "Vitória").
-capital("Goiás", "Goiânia").
-capital("Maranhão", "São Luís").
-capital("Mato Grosso", "Cuiabá").
-capital("Mato Grosso do Sul", "Campo Grande").
-capital("Minas Gerais", "Belo Horizonte").
-capital("Pará", "Belém").
-capital("Paraíba", "João Pessoa").
-capital("Paraná", "Curitiba").
-capital("Pernambuco", "Recife").
-capital("Piauí", "Teresina").
-capital("Rio de Janeiro", "Rio de Janeiro").
-capital("Rio Grande do Norte", "Natal").
-capital("Rio Grande do Sul", "Porto Alegre").
-capital("Rondônia", "Porto Velho").
-capital("Roraima", "Boa Vista").
-capital("Santa Catarina", "Florianópolis").
-capital("São Paulo", "São Paulo").
-capital("Sergipe", "Aracaju").
-capital("Tocantins", "Palmas").
+capital("acre", "rio branco").
+capital("alagoas", "maceió").
+capital("amapá", "macapá").
+capital("amazonas", "manaus").
+capital("bahia", "salvador").
+capital("ceará", "fortaleza").
+capital("distrito federal", "brasília").
+capital("espírito santo", "vitória").
+capital("goiás", "goiânia").
+capital("maranhão", "são luís").
+capital("mato grosso", "cuiabá").
+capital("mato grosso do sul", "campo grande").
+capital("minas gerais", "belo horizonte").
+capital("pará", "belém").
+capital("paraíba", "joão pessoa").
+capital("paraná", "curitiba").
+capital("pernambuco", "recife").
+capital("piauí", "teresina").
+capital("rio de janeiro", "rio de janeiro").
+capital("rio grande do norte", "natal").
+capital("rio grande do sul", "porto alegre").
+capital("rondônia", "porto velho").
+capital("roraima", "boa vista").
+capital("santa catarina", "florianópolis").
+capital("são paulo", "são paulo").
+capital("sergipe", "aracaju").
+capital("tocantins", "palmas").
+
+comida("sudeste", ["feijoada","moqueca capixaba"]).
+comida("norte", ["tacacá","pato no tucupi"]).
+comida("sul", ["chimarrão","churrasco", "barreado"]).
+comida("centro-oeste", ["chambaril","frango com pequi"]).
+comida("nordeste", ["bolo de macaxeira","tapioca"]).
+
+tamanho("amazonas",1570.7).
+tamanho("pará",1247.6).
+tamanho("mato grosso",903.3).
+tamanho("minas gerais",587.5).
+tamanho("bahia",564.6).
+tamanho("mato grosso do sul",357.1).
+tamanho("goiás",340.0).
+tamanho("maranhão",332.0).
+tamanho("rio grande do sul",281.7).
+tamanho("tocantins",277.6).
+tamanho("piauí",251.5).
+tamanho("são paulo",248.2).
+tamanho("rondônia",237.5).
+tamanho("roraima",224.2).
+tamanho("paraná",199.3).
+tamanho("acre",152.5).
+tamanho("ceará",148.8).
+tamanho("amapá",142.8).
+tamanho("pernambuco",98.3).
+tamanho("santa catarina",95.3).
+tamanho("paraíba",56.4).
+tamanho("rio grande do norte",52.8).
+tamanho("espírito santo",46.0).
+tamanho("rio de janeiro",43.6).
+tamanho("alagoas",27.7).
+tamanho("sergipe",21.9).
+tamanho("distrito federal",5.8).
+
+pib("acre","R$ 13,4 ").
+pib("alagoas","R$ 40,9 ").
+pib("amapá","R$ 13,4 ").
+pib("amazonas","R$ 86,6 ").
+pib("bahia","R$ 223,9 ").
+pib("ceará","R$ 126,0 ").
+pib("distrito federal","R$ 197,4 ").
+pib("espírito santo","R$ 128,7 ").
+pib("goiás","R$ 165,0 ").
+pib("maranhão","R$ 76,8 ").
+pib("mato grosso","R$ 101,2 ").
+pib("mato grosso do sul","R$ 78,9 ").
+pib("minas gerais","R$ 516,6 ").
+pib("pará","R$ 124,5 ").
+pib("paraíba","R$ 52,9 ").
+pib("paraná","R$ 348,0 ").
+pib("pernambuco","R$ 155,1 ").
+pib("piauí","R$ 37,7 ").
+pib("rio de janeiro","R$ 671,0 ").
+pib("rio grande do norte","R$ 54,0 ").
+pib("rio grande do sul","R$ 357,8 ").
+pib("rondônia","R$ 34,0 ").
+pib("roraima","R$ 9,7 ").
+pib("santa catarina","R$ 242,5 ").
+pib("são paulo","R$ 1.858,1 trilhão").
+pib("sergipe","R$ 37,4 ").
+pib("tocantins","R$ 26,1 ").
